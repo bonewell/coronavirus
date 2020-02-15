69288936363b9558ebb65fc1d215c85d7a972f9a
Konstantin Kolodiy <bonewell@yandex.ru>
2020-02-15 21:48:40 +0300

    Parse command line arguments

--- main/main.cpp
+++ main/main.cpp
@@ -1,3 +1,5 @@
+#include <QCommandLineParser>
+#include <QDebug>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
@@ -9,6 +11,41 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+    QGuiApplication::setApplicationName("CoronaVirus");
+    QGuiApplication::setApplicationVersion("1.0");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("Simulates live of the viruses");
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addOptions({
+        {{"q", "quantity"}, "Quantity of the polygons on start. Default is 1", "quantity", "1"},
+        {{"r", "radius"}, "Size of the polygon in pixels. Default is 50", "radius", "50"},
+        {{"l", "lifetime"}, "Lifetime of the polygon in milliseconds. Default is 1000", "lifetime", "1000"},
+    });
+    parser.addPositionalArgument("min", "Minimum number of the sides of the polygon");
+    parser.addPositionalArgument("max", "Maximum number of the sides of the polygon");
+    parser.process(app);
+
+    auto quantity = parser.value("quantity").toInt();
+    auto radius = parser.value("radius").toDouble();
+    auto lifetime = parser.value("lifetime").toInt();
+    auto strings = parser.positionalArguments();
+
+    if (strings.length() < 2) qFatal("min and max should be specified");
+    auto limits = std::make_pair<int, int>(strings.at(0).toInt(), strings.at(1).toInt());
+
+    if (limits.first < 3) qFatal("min should be greater 3");
+    if (limits.first > limits.second) qFatal("min should be less max");
+
+    qDebug() << "quantity=" << quantity;
+    qDebug() << "radius=" << radius;
+    qDebug() << "lifetime=" << lifetime;
+    qDebug() << "min=" << limits.first;
+    qDebug() << "max=" << limits.second;
+
+    RegularPolygonModel model;
+    model.addPolygon({{250, 250}, radius, limits.second});
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
@@ -17,11 +54,8 @@ int main(int argc, char *argv[])
         if (!obj && url == objUrl)
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
-    engine.load(url);
-
-    RegularPolygonModel model;
-    model.addPolygon({{250, 250}, 200, 7});
     engine.rootContext()->setContextProperty("polygons", &model);
+    engine.load(url);
 
     return app.exec();
 }
--- main/main.pro
+++ main/main.pro
@@ -2,6 +2,8 @@ QT += quick
 
 CONFIG += c++11
 
+TARGET = coronavirus
+
 # The following define makes your compiler emit warnings if you use
 # any Qt feature that has been marked deprecated (the exact warnings
 # depend on your compiler). Refer to the documentation for the
